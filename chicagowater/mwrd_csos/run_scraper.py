de206572b1e854418e377d3a133426386dc295f9
sbeslow <bezzy21@yahoo.com>
2016-02-09 20:22:59 -0600

    Looping through times

--- chicagowater/mwrd_csos/run_scraper.py
+++ chicagowater/mwrd_csos/run_scraper.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 
 from mwrd_csos.database_methods import insert_cso_into_db, select_from_db
 from mwrd_csos.helpers import find_overlapping_times
@@ -28,6 +28,17 @@ def scrape_by_date(date_str):
     conn.close()
 
 
+def scrape_for_range(start_date_str, end_date_str):
+
+    iter = datetime.datetime.strptime(start_date_str, "%m/%d/%Y")
+    end = datetime.datetime.strptime(end_date_str, "%m/%d/%Y")
+
+    while iter <= end:
+        date = iter.strftime("%m/%d/%Y")
+        print date
+        iter += datetime.timedelta(days=1)
+
+
 def search_for_overlap(event):
     conn = sqlite3.connect(sqlite_file)
     c = conn.cursor()
@@ -37,9 +48,7 @@ def search_for_overlap(event):
     return a
 
 
-event = {"date": "2015-9-18", "location": 'DS-M15',
-         "start": datetime.strptime("3:30", "%H:%M"),
-         "stop": datetime.strptime("3:45", "%H:%M"),}
-a = search_for_overlap(event)
-for b in a:
-    print("ID=" + str(b["id"]))
+# event = {"date": "2015-9-18", "location": 'DS-M15',
+#          "start": datetime.strptime("3:30", "%H:%M"),
+#          "stop": datetime.strptime("3:45", "%H:%M"),}
+scrape_for_range("01/02/2012", "05/01/2012")
